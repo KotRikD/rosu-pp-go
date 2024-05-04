9f610a102460de54ebfb777ed4320c2c8859ff73
Yoru <m.f.fetvaci@hotmail.com>
2024-05-04 16:07:09 +0300

    format main.go

--- main.go
+++ main.go
@@ -13,23 +13,23 @@ import (
 )
 
 type ScoreParams struct {
-    mode 			uint
-    mods 			uint
-    acc 			float64
-    n300 			uint
-    n100 			uint
-    n50 			uint
-    nMisses 			uint
-    nKatu 			uint
-    combo 			uint
-    score 			uint
-    passedObjects 		uint
-    clockRate 			float64
+	mode          uint
+	mods          uint
+	acc           float64
+	n300          uint
+	n100          uint
+	n50           uint
+	nMisses       uint
+	nKatu         uint
+	combo         uint
+	score         uint
+	passedObjects uint
+	clockRate     float64
 }
 
 type RosuCalculator struct {
-	mapPath 	string
-	scoreParams	ScoreParams
+	mapPath     string
+	scoreParams ScoreParams
 }
 
 func (rosu RosuCalculator) Calculate() C.calculateresult {
@@ -43,36 +43,36 @@ func (rosu RosuCalculator) Calculate() C.calculateresult {
 	var scoreParams *C.scoreparams
 	C.score_params_new(&scoreParams)
 	C.score_params_mode(scoreParams, C.mode(rosu.scoreParams.mode))
-	if (rosu.scoreParams.mods > 0) {
+	if rosu.scoreParams.mods > 0 {
 		C.score_params_mods(scoreParams, C.uint(rosu.scoreParams.mods))
 	}
-	if (rosu.scoreParams.acc > 0) {
+	if rosu.scoreParams.acc > 0 {
 		C.score_params_acc(scoreParams, C.double(rosu.scoreParams.acc))
 	}
-	if (rosu.scoreParams.n300 > 0) {
+	if rosu.scoreParams.n300 > 0 {
 		C.score_params_n300(scoreParams, C.uint(rosu.scoreParams.n300))
 	}
-	if (rosu.scoreParams.n100 > 0) {
+	if rosu.scoreParams.n100 > 0 {
 		C.score_params_n100(scoreParams, C.uint(rosu.scoreParams.n100))
 	}
-	if (rosu.scoreParams.n50 > 0) {
+	if rosu.scoreParams.n50 > 0 {
 		C.score_params_n50(scoreParams, C.uint(rosu.scoreParams.n50))
 	}
-	if (rosu.scoreParams.combo > 0) {
+	if rosu.scoreParams.combo > 0 {
 		C.score_params_combo(scoreParams, C.uint(rosu.scoreParams.combo))
 	}
-	if (rosu.scoreParams.nMisses > 0) {
+	if rosu.scoreParams.nMisses > 0 {
 		C.score_params_n_misses(scoreParams, C.uint(rosu.scoreParams.nMisses))
 	}
-	if (rosu.scoreParams.nKatu > 0) {
+	if rosu.scoreParams.nKatu > 0 {
 		C.score_params_n_katu(scoreParams, C.uint(rosu.scoreParams.nKatu))
-	} 
-	if (rosu.scoreParams.passedObjects > 0) {
+	}
+	if rosu.scoreParams.passedObjects > 0 {
 		C.score_params_passed_objects(scoreParams, C.uint(rosu.scoreParams.nKatu))
 	}
-	if (rosu.scoreParams.clockRate > 0) {
+	if rosu.scoreParams.clockRate > 0 {
 		C.score_params_clock_rate(scoreParams, C.double(rosu.scoreParams.clockRate))
-	} 
+	}
 	defer C.score_params_destroy(&scoreParams)
 
 	calculationResult := C.calculator_calculate(calculator, scoreParams)
@@ -83,14 +83,14 @@ func main() {
 	calculator := RosuCalculator{
 		mapPath: "./test.osu",
 		scoreParams: ScoreParams{
-			mode: 0,
-			n300: 372,
-			n100: 8,
-			n50: 0,
+			mode:    0,
+			n300:    372,
+			n100:    8,
+			n50:     0,
 			nMisses: 0,
-			acc: 98.60,
-			combo: 542,
-			mods: 72,
+			acc:     98.60,
+			combo:   542,
+			mods:    72,
 		},
 	}
 	result := calculator.Calculate()
